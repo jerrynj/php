5543c95510ee4f7fe948d723ecfadd75a210ff38
jerrynj <xiang37202@163.com>
2018-05-23 23:28:18 +0800

    init commit

---
+++ fastadmin/application/api/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//配置文件
+return [
+    'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
+
+    'HLY_AUTH_URL' => 'http://183.207.215.209:8088/bss/auth.html',
+    'HLY_GETNUM_URL' => 'http://183.207.215.209:8088/bss/getNum.html',
+    'HLY_LOCKNUM_URL' => 'http://183.207.215.209:8088/bss/lockNum.html',
+    'HLY_UNLOCK_URL' => 'http://183.207.215.209:8088/bss/unlockNum.html',
+    'HLY_IDENCHECK_URL' => 'http://183.207.215.209:8088/bss/idenCheck.html',
+    'HLY_ORDER_URL' => 'http://183.207.215.209:8088/bss/order.html',
+    'HLY_QUERY_URL' => 'http://183.207.215.209:8088/bss/query.html',
+    'HLY_ORDERCANCEL_URL' => 'http://183.207.215.209:8088/bss/ordercancel.html',
+
+    'AppKey' => '13af4043889f48868e75a84f3e3b2505', // API Key
+    'SecretKey' => "454193dc17c54752a47b539ee526bcb0", // Secret Key
+];
